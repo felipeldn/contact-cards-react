924d9a38b1002e7c5279a72879e2df6e4d48f90d
felipeldn91 <felipeldn91@gmail.com>
2021-06-23 03:32:38 +0100

    React practice contact cards

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ContactCard from './ContactCard.js';
+
+function App() {
+  return (
+    <div >
+      <ContactCard/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/ContactCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ContactCard() {
+    return (
+        <div className="contact-card">
+            <img src="http://placekitten.com/300/200/"/>
+            <h3>Mr. Whiskerson</h3>
+            <p>Phone: 07756479809</p>
+            <p>Email: cat1@mail.com</p>
+        </div>
+    )
+}
+
+export default ContactCard
